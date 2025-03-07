dd5ce591b370b85491682dbe571dc8589252c7f3
Mustard4868 <nynkelindeman@gmail.com>
2025-03-07 18:18:22 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# Remove yay folder
+cd .. && rm -rf yay
+
+# Install packages
+yay -S $(cat dependencies)
+
+stow wallpapers --adopt && stow hyprland --adopt
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+source ~/.zshrc
+
+# Install Homebrew packages
+brew install gcc oh-my-posh zsh-autosuggestions
+
